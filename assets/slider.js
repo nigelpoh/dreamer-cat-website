d2a8f0ddb54cfb3830047845c2b258a28b38b224
Nigel <nigelpoh@Nigels-MacBook-Pro-2.local>
2024-11-07 15:45:52 +0800

    feat: improvements done to product-media-gallery and product-thumbnail

--- assets/slider.js
+++ assets/slider.js
@@ -1,2 +1,24 @@
-// var selector = "#thumbnail-" + document.querySelector("#slider-controller").value
-// document.querySelector(selector).classList.remove("hidden")
+var allScrollables = document.querySelectorAll('[id^=ScrollableGallery]');
+
+allScrollables.forEach(scrollable => {
+    scrollable.addEventListener("scroll", (e) => {
+        var current = e.currentTarget.scrollLeft / e.currentTarget.scrollWidth
+        var total = (e.currentTarget.childElementCount - 1) / e.currentTarget.childElementCount
+        var index = Math.round((e.currentTarget.childElementCount - 1) * current / total)
+        var sectionSplit = scrollable.id.split("-")
+        var sectionID = sectionSplit.slice(1, sectionSplit.length).join("-") + "-"
+        if (index != 0) {
+            var colour = document.querySelector('#SliderIndicator-' + sectionID + (index - 1).toString()).getAttribute('data-colour')
+            document.querySelector('#SliderIndicator-' + sectionID + (index - 1).toString()).classList.add("w-4", "bg-neutral-400")
+            document.querySelector('#SliderIndicator-' + sectionID + (index - 1).toString()).classList.remove("w-14", colour)
+        }
+        if (index != e.currentTarget.childElementCount - 1) {
+            var colour = document.querySelector('#SliderIndicator-' + sectionID + (index + 1).toString()).getAttribute('data-colour')
+            document.querySelector('#SliderIndicator-' + sectionID + (index + 1).toString()).classList.add("w-4", "bg-neutral-400")
+            document.querySelector('#SliderIndicator-' + sectionID + (index + 1).toString()).classList.remove("w-14", colour)
+        }
+        var colour = document.querySelector('#SliderIndicator-' + sectionID + index.toString()).getAttribute('data-colour')
+        document.querySelector('#SliderIndicator-' + sectionID + index.toString()).classList.remove("w-4", "bg-neutral-400")
+        document.querySelector('#SliderIndicator-' + sectionID + index.toString()).classList.add("w-14", colour)
+    }); 
+});
