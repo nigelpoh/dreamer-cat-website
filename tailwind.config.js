c38097f6a5bab82aa0a5af4b6e534db00bae1b4d
Nigel <nigelpoh@Nigels-MacBook-Pro-2.local>
2024-11-13 23:33:38 +0800

    fix: multiple UI fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -53,6 +53,9 @@ module.exports = {
           '0%, 100%': { transform: 'rotate(-3deg)' },
           '50%': { transform: 'rotate(3deg)' },
         }
+      },
+      gridTemplateRows: {
+        'cover-all': '1fr min-content',
       }
     },
   },
